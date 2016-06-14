6340acdfcc0128560641c3a34ba2a3c8db8b7da0
Daniel Buchner <danieljb2@gmail.com>
2016-06-13 17:24:58 -0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,24 +1,22 @@
-(function(){  
+(function(){
+  var bounce = function(){
+    var args = this.queueArgs;
+    var target = this.queueTarget;
+    this.queueArgs = this.queueTarget = this.queueRequest = null;
+    return this.apply(target, args);
+  };
 
-  // Create your component here
-  // http://x-tags.org/docs
-  
-  xtag.register('x-stub', {
-    lifecycle: {
-      created: function() {},
-      inserted: function() {},
-      removed: function() {},
-      attributeChanged: function() {}
-    }, 
-    events: { 
-    
-    },
-    accessors: {
-      
-    }, 
-    methods: {
-      
+  xtag.pseudos.debounce = {
+    onCompiled: function(fn, pseudo){
+      var timer = pseudo.arguments[0] == 'timer';
+      var duration = pseudo.arguments[1];
+      return function(){
+        fn.queueArgs = arguments;
+        fn.queueTarget = this;
+        fn.queueRequest = fn.queueRequest || (timer ?
+                          setTimeout(bounce.bind(fn), duration || 100) :
+                          xtag.requestFrame(bounce.bind(fn)));
+      }
     }
-  });
-
+  }
 })();
